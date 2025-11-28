e9f5364641a02819dc329c638afc51711d6af34c
Jason  Rudder <37918719+rudder-research@users.noreply.github.com>
2025-11-28 09:48:38 -0500

    Add files via upload

---
+++ core/math/__init__.py
@@ -0,0 +1,10 @@
+"""
+VCF Mathematical Framework
+Hamiltonian mechanics, KAM theory, differential geometry
+"""
+
+from .vcf_geometry import *
+from .vcf_coherence import *
+from .vcf_normalization import *
+
+__all__ = ['vcf_geometry', 'vcf_coherence', 'vcf_normalization']
